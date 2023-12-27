c29c0d948add468c6568f277da8eef0f9e45ae08
MyoMinnNaing <myominnaing.webdev@gmail.com>
2023-12-27 09:18:29 +0630

    againg

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,13 @@ import React from "react";
 import Wrapper from "./components/UI/Wrapper";
 import Header from "./components/header/Header";
 import CreativeProject from "./components/CreativeProject";
-import Slider from "./components/UI/Slider";
 
 const App = () => {
   return (
     <Wrapper>
       <Header />
 
-      {/* <CreativeProject /> */}
-
-      <Slider />
+      <CreativeProject />
     </Wrapper>
   );
 };
